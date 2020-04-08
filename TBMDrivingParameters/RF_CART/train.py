10339aea557ad94eac232d167f59e7f97d7b5ec4
shizi9 <971883270@qq.com>
2020-04-08 11:12:50 +0800

    和数据预处理同学对接，将输出写入log.txt

--- TBMDrivingParameters/RF_CART/train.py
+++ TBMDrivingParameters/RF_CART/train.py
@@ -10,18 +10,20 @@ import joblib
 import csv
 import os
 import sys
+# import matplotlib.pyplot as plt
 
 path = os.path.abspath(os.path.dirname(sys.argv[0]))
 feature = []
 result_f = []
 result_t = []
 data_file = csv.reader(open(path + '/data/train.csv'))
+next(data_file)
 for content in data_file:
     content = list(map(float, content))
     if len(content) != 0:
         feature.append(content[0:14])
-        result_f.append(content[14])
-        result_t.append(content[15])
+        result_f.append(content[15])
+        result_t.append(content[14])
 scaler = StandardScaler()
 scaler.fit(feature)
 feature = scaler.transform(feature)
@@ -31,31 +33,63 @@ feature_train_f, feature_test_f, result_train_f, result_test_f = \
 feature_train_t, feature_test_t, result_train_t, result_test_t = \
     train_test_split(feature, result_t, test_size=0.2, random_state=0)
 
-clf_f = RandomForestRegressor()
-clf_t = RandomForestRegressor()
+# scores_f = []
+# for i in range(0, 200, 10):
+#     rf = RandomForestRegressor(n_estimators=i + 1)
+#     rf.fit(feature_train_f, result_train_f)
+#     predict_result_f = rf.predict(feature_test_f)
+#     score = explained_variance_score(predict_result_f, result_test_f)
+#     scores_f.append(score)
+# print(max(scores_f))
+# plt.plot(range(1, 201, 10), scores_f)
+# plt.show()
+
+# scores_t = []
+# for i in range(0, 200, 10):
+#     rf = RandomForestRegressor(n_estimators=i + 1)
+#     rf.fit(feature_train_t, result_train_t)
+#     predict_result_t = rf.predict(feature_test_t)
+#     score = explained_variance_score(predict_result_t, result_test_t)
+#     scores_t.append(score)
+# print(max(scores_t))
+# plt.plot(range(1, 201, 10), scores_t)
+# plt.show()
+
+clf_f = RandomForestRegressor(n_estimators=20, n_jobs=-1, oob_score=True)
+clf_t = RandomForestRegressor(n_estimators=20, n_jobs=-1, oob_score=True)
 clf_f.fit(feature_train_f, result_train_f)
 clf_t.fit(feature_train_t, result_train_t)
 predict_result_f = clf_f.predict(feature_test_f)
 predict_result_t = clf_t.predict(feature_test_t)
-print('explained_variance_score of F',
-      explained_variance_score(predict_result_f, result_test_f))
-print('explained_variance_score of T',
-      explained_variance_score(predict_result_t, result_test_t))
-print('mean_squared_error of F:',
-      mean_squared_error(predict_result_f, result_test_f))
-print('mean_squared_error of T:',
-      mean_squared_error(predict_result_t, result_test_t))
-print('mean_squared_log_error of F',
-      mean_squared_log_error(predict_result_f, result_test_f))
-print('mean_squared_log_error of T',
-      mean_squared_log_error(predict_result_t, result_test_t))
-print('median_absolute_error of F',
-      median_absolute_error(predict_result_f, result_test_f))
-print('median_absolute_error of T',
-      median_absolute_error(predict_result_t, result_test_t))
-print('mean_absolute_error of F',
-      mean_absolute_error(predict_result_f, result_test_f))
-print('mean_absolute_error of T',
-      mean_absolute_error(predict_result_t, result_test_t))
+log_file = open((path + '/log.txt'), mode='w')
+log_file.write('explained_variance_score of F: ' +
+               str(explained_variance_score(predict_result_f, result_test_f)) + 
+               '\n')
+log_file.write('explained_variance_score of T: ' +
+               str(explained_variance_score(predict_result_t, result_test_t)) + 
+               '\n')
+log_file.write('mean_squared_error of F: ' +
+               str(mean_squared_error(predict_result_f, result_test_f)) + '\n')
+log_file.write('mean_squared_error of T: ' +
+               str(mean_squared_error(predict_result_t, result_test_t)) + '\n')
+log_file.write('mean_squared_log_error of F: ' +
+               str(mean_squared_log_error(predict_result_f, result_test_f)) +
+               '\n')
+log_file.write('mean_squared_log_error of T: ' +
+               str(mean_squared_log_error(predict_result_t, result_test_t)) +
+               '\n')
+log_file.write('median_absolute_error of F: ' +
+               str(median_absolute_error(predict_result_f, result_test_f)) +
+               '\n')
+log_file.write('median_absolute_error of T: ' +
+               str(median_absolute_error(predict_result_t, result_test_t)) +
+               '\n')
+log_file.write('mean_absolute_error of F: ' +
+               str(mean_absolute_error(predict_result_f, result_test_f)) +
+               '\n')
+log_file.write('mean_absolute_error of T: ' +
+               str(mean_absolute_error(predict_result_t, result_test_t)) +
+               '\n')
+log_file.close()
 joblib.dump(clf_f, path + "/model/model_f.pkl")
 joblib.dump(clf_t, path + "/model/model_t.pkl")
