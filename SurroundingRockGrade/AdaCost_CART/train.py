73df70eedcd74fcfc9e767b8b29e29e2aa4d6eef
CD34239 <1174887468@qq.com>
2020-04-13 14:33:07 +0800

    minor fixes

--- SurroundingRockGrade/AdaCost_CART/train.py
+++ SurroundingRockGrade/AdaCost_CART/train.py
@@ -15,6 +15,7 @@ X = df[['刀盘运行时间均值', '撑靴压力均值', '刀盘转速均值', 
         '左撑靴滚动角均值', '左撑靴油缸行程检测均值', '右撑靴滚动角均值', '右撑靴油缸行程检测均值']].values
 y = df['围岩等级'].values
 
+# 代价矩阵
 cost_matrix = [[0, 0, 0, 0, 0, 0],
                [0, 0, 0, 0, 0, 0],
                [0, 0, 0, 4, 2, 1],
@@ -22,22 +23,24 @@ cost_matrix = [[0, 0, 0, 0, 0, 0],
                [0, 0, 2, 4, 0, 1],
                [0, 0, 2, 6, 4, 0]]
 
+# 十折交叉验证
 kf = KFold(n_splits=10)
+# model
 adaCost = None
-
+# 记录每次迭代的数据
 train_accuracy_with_n_est = []
 test_accuracy_with_n_est = []
 recall_with_n_est = []
 f1_with_n_est = []
 class_recall_with_n_est = [[], [], [], []]
 # model_pkl = None
-for n_est in range(1, 200):
+for n_est in range(1, 150):
     test_accuracy = 0.0
     train_accuracy = 0.0
     recall = 0.0
     f1 = 0.0
     class_recall = []
-    # print("n_est", n_est)
+    print("使用", n_est, "个弱分类器：")
     for train_index, test_index in kf.split(X):
         adaCost = AdaCostClassifier(
             DecisionTreeClassifier(max_depth=5),
@@ -50,6 +53,7 @@ for n_est in range(1, 200):
         # print(accuracy_score(y, adaCost.predict(X)))
         true = y[test_index]
         pred = adaCost.predict(X[test_index])
+        # 准确率 召回率 f1-score 分类召回率
         test_accuracy += accuracy_score(true, pred) / 10.0
         train_accuracy += accuracy_score(y[train_index], adaCost.predict(X[train_index])) / 10.0
         f1 += f1_score(true, pred, average='macro') / 10.00
@@ -61,6 +65,7 @@ for n_est in range(1, 200):
                                                                             labels=[2, 3, 4, 5], average=None)
         class_recall.append(r_class)
 
+    # 计算各类的平均召回率，如果该类没有出现，不考虑计入平均值
     for class_n in range(4):
         count = 0
         sum = 0.0
@@ -78,6 +83,23 @@ for n_est in range(1, 200):
     recall_with_n_est.append(recall)
     f1_with_n_est.append(f1)
 
+# 各个指标写入文件
+file = open(path + '/image/test_accuracy_with_n_est.txt','w')
+file.write(str(test_accuracy_with_n_est))
+file.close()
+file = open(path + '/image/train_accuracy_with_n_est.txt','w')
+file.write(str(train_accuracy_with_n_est))
+file.close()
+file = open(path + '/image/recall_with_n_est.txt','w')
+file.write(str(recall_with_n_est))
+file.close()
+file = open(path + '/image/f1_with_n_est.txt','w')
+file.write(str(f1_with_n_est))
+file.close()
+file = open(path + '/image/class_recall_with_n_est.txt','w')
+file.write(str(class_recall_with_n_est))
+file.close()
+
 
 s = pickle.dumps(adaCost)
 f = open(path + '/model/adaCost.pickle', 'wb+')
@@ -88,8 +110,9 @@ f.close()
 # print(test_accuracy_with_n_est)
 # print(recall_with_n_est)
 # print(f1_with_n_est)
-axis = [i for i in range(1, 200)]
 
+# 图标的横轴 弱分类器的数量
+axis = [i for i in range(1, 150)]
 plt.figure(1)
 # 将第一个画板划分为2行1列组成的区块，并获取到第一块区域
 ax1 = plt.subplot(211)
