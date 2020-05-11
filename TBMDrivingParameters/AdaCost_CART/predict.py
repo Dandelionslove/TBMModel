30fc7a56bb2a14148d93b2e36be3269ae7eb4f32
ZexuHan <39207097+hanzexu66@users.noreply.github.com>
2020-05-11 15:01:32 +0800

    no message

---
+++ TBMDrivingParameters/AdaCost_CART/predict.py
@@ -0,0 +1,16 @@
+import sys
+import os
+import json
+import pickle
+import numpy as np
+
+def adaCost(sample):
+    # 可以接受（样本数X特征数）这样的二维ndarray，也可以接受只有一个样本的一维数组（会自动转换为只有一行的二维数组）
+    # 输出一个一维ndarray，第i个元素（数字）代表第i个样本预测的围岩等级。
+    path = os.path.abspath(os.path.dirname(sys.argv[0]))
+    f = open(path + '/model/adaCost.pickle', 'rb')
+    s = f.read()
+    model = pickle.loads(s)
+    if len(sample.shape) == 1:
+        sample = sample.reshape(1, len(sample))
+    return model.predict(sample)
