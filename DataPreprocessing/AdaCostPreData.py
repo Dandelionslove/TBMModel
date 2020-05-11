80b48b790889167522d50a9a62822afe01b6d0a1
handofwatch <3415858760@qq.com>
2020-05-11 19:20:42 +0800

    使用论文中对上升段的新定义

--- DataPreprocessing/AdaCostPreData.py
+++ DataPreprocessing/AdaCostPreData.py
@@ -1,6 +1,5 @@
-from DataPreprocessing import AllUnZip
+# from DataPreprocessing import AllUnZip
 from DataPreprocessing import AdaCost
-import pandas as pd
 # AllUnZip()
 adaCost = AdaCost()
 adaCost.adaCostData()
--- DataPreprocessing/AllPreData.py
+++ DataPreprocessing/AllPreData.py
@@ -1,7 +1,6 @@
-from DataPreprocessing import AllUnZip
+# from DataPreprocessing import AllUnZip
 from DataPreprocessing import RF
 from DataPreprocessing import AdaCost
-import pandas as pd
 # AllUnZip()
 rf = RF()
 rf.RFData()
