f772b597bd9822d2d59e29856ea256a69ee368aa
shizi9 <971883270@qq.com>
2020-05-14 08:28:11 +0800

    Update GB.py

--- TBMDrivingParameters/RF_CART/GB.py
+++ TBMDrivingParameters/RF_CART/GB.py
@@ -54,11 +54,6 @@ param = {
     range(2, 10, 1),
     'min_samples_leaf':
     range(1, 10, 1),
-    'learning_rate': [
-        0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19,
-        0.20, 0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29,
-        0.30
-    ],
 }
 
 gcv_t = GridSearchCV(estimator=GradientBoostingRegressor(),
